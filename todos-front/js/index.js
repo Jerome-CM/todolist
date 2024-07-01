714bd5720965b6c326b0b2f13d2bbaba3b2120c6
Jérôme <100235233+Jerome-CM@users.noreply.github.com>
2024-07-01 21:43:30 +0200

    origin push

---
+++ todos-front/js/index.js
@@ -0,0 +1,17 @@
+function validateForm(event){
+    event.preventDefault();
+
+    let x = document.forms["form"]["firstname"].value;
+    console.log(x);
+    if (x == "") {
+        let divError = document.getElementById("errorMessage");
+        divError.classList.add('alert', 'alert-danger');
+        divError.innerHTML = "Please, entre your firstname";
+
+        return false;
+    } else {
+        localStorage.setItem("firstname", x);
+        console.log("inStorage : " + localStorage.getItem("firstname"));
+        window.location.href = "../item.html";
+    }
+}
